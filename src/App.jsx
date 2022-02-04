4d8720ced73dfa244903e506bfb5fc0e5b77b21d
abizmo <abizmo@gmail.com>
2022-02-04 10:03:29 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Victor Crest
+    26
+    London
+
+    80K
+    Followers
+
+    803K
+    Likes
+
+    1.4K
+    Photos
+  </div>
+);
+
+export default App;
